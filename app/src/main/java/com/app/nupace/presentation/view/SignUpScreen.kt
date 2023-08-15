72e63225ce365ff37db50d9dc3180a600cf330c0
withcarlox <withcarlox@gmail.com>
2023-08-15 16:55:26 +0100

    add activities

---
+++ app/src/main/java/com/app/nupace/presentation/view/SignUpScreen.kt
@@ -0,0 +1,12 @@
+package com.app.nupace.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.sprintspace.R
+
+class SignUpScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_screen)
+    }
+}
